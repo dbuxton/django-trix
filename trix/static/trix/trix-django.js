3253374f5c5f6b4621cd05897ad5d1088162332e
David Buxton <david@dbuxton.com>
2015-12-22 12:22:18 +0000

    Remove default event listener for attachments

--- trix/static/trix/trix-django.js
+++ trix/static/trix/trix-django.js
@@ -3,14 +3,14 @@
 // });
 
 
-document.addEventListener('trix-attachment-add', function(ev) {
+// document.addEventListener('trix-attachment-add', function(ev) {
 
-  var data = {};
+//   var data = {};
 
-  var request = new XMLHttpRequest();
-  request.open('POST', '/trix/attachment/', true);
-  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-  request.send(data);
+//   var request = new XMLHttpRequest();
+//   request.open('POST', '/trix/attachment/', true);
+//   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+//   request.send(data);
 
-});
+// });
 
